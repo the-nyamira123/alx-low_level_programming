e5868fd910db3e5cbe091a7663acc3220681dc75
the-nyamira123 <nathanslay7@gmail.com>
2023-11-22 05:02:45 -0800

    function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -35,5 +35,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
 size_t free_listint_safe(listint_t **h);
+void free_listp(listp_t **head);
 
 #endif /* NYAMIRA BABY */
